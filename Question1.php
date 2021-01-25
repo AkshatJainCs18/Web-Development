5b2dc936a30488903174575283f3c31a6a3a82a4
AkshatJainCs18 <57089291+AkshatJainCs18@users.noreply.github.com>
2021-01-25 14:59:52 +0530

    5th Commit(Read Description)
    Fixed Syntax Error on Line 63
    Added Javascript Based Validation for Number of Interests instead of php Based Validation
    Added table as requested for output.
    Fixed few more typos here and there

--- Question1.php
+++ Question1.php
@@ -7,12 +7,36 @@ if (isset($_POST['submit'])) {
     $course = $_POST['Course'];
     $interest = $_POST['interest'];
    
-    echo "Name : $name <br/>";
-        echo "Email : $email <br/>";
-        echo "Contact : $contact <br/>";
-        echo "Course : $course <br/>";
-    if (sizeof($interest) < 3||sizeof($interest)>5) {
-        echo "At least select any 3 and at max 5 Interests.";}}
+    //echo "Name : $name <br/>";
+       // echo "Email : $email <br/>";
+        //echo "Contact : $contact <br/>";
+        //echo "Course : $course <br/>";
+    //if (sizeof($interest) < 3||sizeof($interest)>5) {
+        //echo "At least select any 3 and at max 5 Interests.";}}
+    echo "
+    <table border='1' >
+    <tr>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Contact</th>
+    <th>City</th>
+    <th>Course></th>
+    <th colspan='3'>Interests</th>
+    </tr>
+    <tr>
+    <td>$name</td>
+    <td>$email</td>
+    <td>$contact</td>
+    <td>$city</td>
+    <td>$course</td>
+    <td>$interest[0]</td>
+    <td>$interest[1]</td>
+    <td>$interest[2]</td>
+     </tr>
+     </table>
+
+
+        ";}
 
     ?>
 
@@ -30,6 +54,16 @@ function validateForm() {
     alert("length must be 10");
     return false;
   }
+  var le =document.getElementsByTagName("input");
+  var c=0;
+  for(var i=0;i<le.length;++i){
+      if(le[i].type=="checkbox" && le[i].checked==true)
+      {
+          c++;
+      }
+  }
+    if(c<3 || c>5)
+    alert("At least 3 interests and at max 5 interests");
   
 }
 </script>
@@ -60,9 +94,9 @@ function validateForm() {
         Programming<input type="checkbox" name="interest[]" value="Programming">
         Cricket<input type="checkbox" name="interest[]" value="Cricket">
         Football<input type="checkbox" name="interest[]" value="Football">
-        Reading<insput type="checkbox" name="interest[]" value="Reading">
+        Reading<input type="checkbox" name="interest[]" value="Reading">
         Singing<input type="checkbox" name="interest[]" value="Singing">
-        Painting<input type="checkbox" name="interest[]" value="Paiting">
+        Painting<input type="checkbox" name="interest[]" value="Painting">
         <br>
 
 
